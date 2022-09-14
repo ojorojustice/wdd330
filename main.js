bf1f678a4a7bf8a42741bac842b59582c6f461eb
ojo19001@byui.edu <ojo19001@byui.edu>
2022-09-15 00:35:36 +0100

    final

--- main.js
+++ main.js
@@ -1,2 +1,93 @@
-"use script";
+"use script"
 
+console.log(frontendDirectory);
+
+const frontendDirectory = [{
+    id : "Week 1",
+    title : "new week"
+    
+
+},
+{
+    id : "Week 2",
+    url : "yyyy"
+    
+
+}
+,
+{
+    id : "Week 3",
+    url : "yyyy"
+    
+
+},
+{
+    id : "Week 4",
+    url : "yyyy"
+    
+
+}
+,
+{
+    id : "Week 5",
+    url : "yyyy"
+    
+
+},
+{
+    id : "Week 6",
+    title : "new week"
+    
+
+},
+{
+    id : "Week 7",
+    url : "yyyy"
+    
+
+}
+,
+{
+    id : "Week 8",
+    url : "yyyy"
+    
+
+},
+{
+    id : "Week 9",
+    url : "yyyy"
+    
+
+}
+,
+{
+    id : "Week 10",
+    url : "yyyy"
+    
+
+},
+{
+    id : "Week 11",
+    title : "new week"
+    
+
+},
+{
+    id : "Week 12",
+    url : "yyyy"
+    
+
+}
+,
+{
+    id : "Week 13",
+    url : "yyyy"
+    
+
+},
+{
+    id : "Week 14",
+    url : "yyyy"
+    
+
+}]
