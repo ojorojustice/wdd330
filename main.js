ebc590ddc9f1c4821b376bbc26b3b456f396f18b
ojo19001@byui.edu <ojo19001@byui.edu>
2022-09-15 02:49:27 +0100

    javascript used

--- main.js
+++ main.js
@@ -1,93 +1,103 @@
-"use script"
 
-console.log(frontendDirectory);
+
+
+/*My objects I will be using these to keep my notes*/
 
 const frontendDirectory = [{
-    id : "Week 1",
-    title : "new week"
-    
-
+    label : "Week 1",
+    title : "Making Computer Remember Me",
+    url: "week1.html",  
 },
 {
-    id : "Week 2",
-    url : "yyyy"
-    
+    label : "Week 2",
+    title : "Programming Basics",
+    url: "week2.html", 
 
 }
 ,
 {
-    id : "Week 3",
-    url : "yyyy"
-    
+    label : "Week 3",
+    title : "Arrays, Logic and Loops",
+    url: "week3.html",     
 
 },
 {
-    id : "Week 4",
-    url : "yyyy"
-    
+    label : "Week 4",
+    title : "functions",
+    url: "week4.html",      
 
 }
 ,
 {
-    id : "Week 5",
-    url : "yyyy"
-    
+    label : "Week 5",
+    title : "Objects",
+    url: "week5.html",     
 
 },
 {
-    id : "Week 6",
-    title : "new week"
-    
+    label : "Week 6",
+    title : "Document Object Model",
+    url: "week6.html",     
 
 },
 {
-    id : "Week 7",
-    url : "yyyy"
-    
+    label : "Week 7",
+    title : "Events",
+    url: "week7.html",  
 
 }
 ,
 {
-    id : "Week 8",
-    url : "yyyy"
-    
-
+    label : "Week 8",
+    title : "Forms",
+    url: "week8.html",  
 },
 {
-    id : "Week 9",
-    url : "yyyy"
-    
-
+    label : "Week 9",
+    title : "Object Oriented Programming",
+    url: "week9.html",  
 }
 ,
 {
-    id : "Week 10",
-    url : "yyyy"
-    
-
+    label : "Week 10",
+    title : "Testing and Debugging",
+    url: "week10.html",  
 },
 {
-    id : "Week 11",
-    title : "new week"
-    
-
+    label : "Week 11",
+    title : "Ajax & Canvas",
+    url: "week11.html",  
 },
 {
-    id : "Week 12",
-    url : "yyyy"
-    
-
+    label: "Week 12",
+    title : "Transforms and Transitions",
+    url: "week12.html",  
 }
 ,
 {
-    id : "Week 13",
-    url : "yyyy"
-    
-
+    label : "Week 13",
+    title : "The Window Object",
+    url: "week13.html",  
 },
 {
-    id : "Week 14",
-    url : "yyyy"
+    label : "Week 14",
+    title : "HTML5 APIS",
+    url: "week14.html",      
+}];
+
+
+const linksList = document.getElementsByClassName('linksList')[0];
+
+console.log(linksList);
+frontendDirectory.forEach((product,index)=>{
+    let atag = document.createElement('a');
+    atag.setAttribute("href", product.url);
+   let linkItem = document.createElement('li');
+   let lintTexts = document.createTextNode(`${product.label}:  ${product.title}`);
+
+    linkItem.appendChild(lintTexts);
+    atag.appendChild(linkItem);
+    linksList.appendChild(atag);
     
 
-}]
+});
