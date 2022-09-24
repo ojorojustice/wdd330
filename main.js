49930270cd7440c5dce4c78ce8aff1f9590c567d
ojo19001@byui.edu <ojo19001@byui.edu>
2022-09-24 13:34:25 +0100

    week2 done

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-
+const quiz = [    ["What is Superman's real name?","Clark Kent"],    ["What is Wonder Woman's real name?","Diana Prince"],    ["What is Batman's real name?","Bruce Wayne"]];
+function start(quiz){    let score = 0;
+    // main game loop    
+    for(const [question,answer] of quiz){        const response = ask(question);        check(response,answer);    }    // end of main game loop
+    gameOver();
+    // function declarations    
+    function ask(question){        return prompt(question);    }
+    function check(response,answer){        if(response === answer){        alert('Correct!');        score++;        } else {        
+        alert(`Wrong! The correct answer was ${answer}`);        }    }
+    function gameOver(){        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);    }}
 
 
 /*My objects I will be using these to keep my notes*/
@@ -7,7 +16,7 @@ const frontendDirectory = [{
     title : "Programming Basics: Making Computer Remember Me (localStorage)",
 },
 {
-    title : "",
+    title : "Arrays, Logic, Loops & Functions",
 }
 ,
 {
@@ -87,3 +96,13 @@ frontendDirectory.forEach((product,index)=>{
 function quizNinja(){
     alert('Welcome to Quiz Ninja!');
 }
+
+console.log(`${[1]===[1]?"yes":"NOooo"}`);
+
+
+const duplicate = [1,2,3,4,5,6,4,3,2,1];
+
+const newset = [];
+
+console.log(newset)
+
