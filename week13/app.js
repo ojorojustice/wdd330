89ba0c242c37dca43eeaece3e260cb22c8c8c20c
ojo19001@byui.edu <ojo19001@byui.edu>
2022-12-16 22:31:36 +0100

    pushing my work online

---
+++ week13/app.js
@@ -0,0 +1,13 @@
+function jensinmi(mia){
+    mia()
+
+    console.log('I will kill ou')
+    mia()
+}
+
+function mia(){
+    console.log('crazy')
+}
+
+
+jensinmi(mia);
---
+++ week14/script.js
@@ -0,0 +1,41 @@
+const navDirectory = [
+    {
+      navTitle: "About Us",
+      navUrl: "about-us.html",
+    },
+    {
+      navTitle: "About Us",
+      navUrl: "about-us.html",
+    },
+    {
+      navTitle: "About Us",
+      navUrl: "about-us.html",
+    },
+    {
+      navTitle: "About Us",
+      navUrl: "about-us.html",
+    },
+  ];
+  
+  const linksList = document.getElementsByClassName("nav__link")[0];
+  
+  console.log(window.URL);
+  
+  navDirectory.forEach((navTab, index) => {
+    let atag = document.createElement("a");
+    atag.setAttribute("href", navTab.navUrl);
+    let linkTexts = document.createTextNode(`${navTab.navTitle}`);
+  
+    //append each of the created elements and nodes
+    atag.appendChild(linkTexts);
+    linksList.appendChild(atag);
+    index++;
+  });
+  
+  const hamburger = document.querySelector(".hamburger");
+  const navLink = document.querySelector(".nav__link");
+  
+  hamburger.addEventListener("click", () => {
+    navLink.classList.toggle("hide");
+  });
+  
